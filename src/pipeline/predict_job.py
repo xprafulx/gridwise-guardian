bad260d785e8a827944b5dbfb3eca052665e520a
xprafulx <pshres25@student.aau.dk>
2026-04-12 15:34:08 +0200

    Change mode from 'TODAY' to 'TOMORROW'

--- src/pipeline/predict_job.py
+++ src/pipeline/predict_job.py
@@ -16,7 +16,7 @@ from src.database.connection import get_db_connection
 
 # ==========================================================
 # 🔄 TOGGLE SWITCH: Change to "TODAY" or "TOMORROW"
-MODE = "TODAY" 
+MODE = "TOMORROW" 
 # ==========================================================
 
 # 🕰️ DANISH TIMEZONE ALIGNMENT
